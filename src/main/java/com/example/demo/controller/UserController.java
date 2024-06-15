210d11ec1f5a2f71280a5e39d47368e5288ca3e2
snehalpati1 <snehalsp99@gmail.com>
2024-06-15 23:30:08 +0530

    commit for for logger ifo given in service api layers and user data also inserted

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserServiceI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api")
 public class UserController {
+
+    Logger log = LoggerFactory.getLogger(UserController.class);
 
     //lagger -log4j
     //sl4j
@@ -31,6 +36,7 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        log.info("Entaring the request for save user data");
         User savedUser = userServiceI.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.OK);
     }
@@ -43,19 +49,21 @@ public class UserController {
 
     @GetMapping("/users/{userId}")
     public ResponseEntity<User> getsingleUser(@PathVariable Long userId) throws Exception {
+        log.info("Entering the request for get user data with userId{}", userId);
         User user = userServiceI.getSingleUser(userId);
+        log.info("Completed the request for get user data with userId{}", userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PutMapping("/users/{userId}")
     public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long userId) {
         User updateUser = userServiceI.updateUser(user, userId);
-        return new  ResponseEntity<>(updateUser,HttpStatus.CREATED);
+        return new ResponseEntity<>(updateUser, HttpStatus.CREATED);
     }
 
-    public  ResponseEntity<String>deleteUser(@PathVariable Long userId){
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
         userServiceI.deleteUser(userId);
-        return  new ResponseEntity<>("Resourse delecte successful",HttpStatus.OK);
+        return new ResponseEntity<>("Resourse delecte successful", HttpStatus.OK);
     }
 }
 
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.demo.service.impl;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserServiceI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserServiceI {
 
+    Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserRepository userRepository;
 
@@ -22,7 +26,9 @@ public class UserServiceImpl implements UserServiceI {
 
     @Override
     public User createUser(User user) {
+        log.info("Initing the dao call for the save user data");
         User savedUser = userRepository.save(user);
+        log.info("Completed the dao call for the save user data");
         return savedUser;
     }
 
@@ -41,8 +47,10 @@ public class UserServiceImpl implements UserServiceI {
     //4th Snehal 28 I am software Dev
     @Override
     public User getSingleUser(Long userId) {
-        userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resourece not found on server"));
-        return null;
+        log.info("Initiating the dao call for get single user data as userId{}:",userId);
+     User user= userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resourece not found on server"));
+        log.info("Complated the dao call for get single user data as userId{}:",userId);
+        return user;
     }
 
     /*Optional<User> user= userRepository.findById(userId);

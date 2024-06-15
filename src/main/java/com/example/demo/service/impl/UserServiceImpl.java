02834bec02c622691fca3cd5debed6e712a381fa
snehalpati1 <snehalsp99@gmail.com>
2024-06-15 15:59:57 +0530

    commit for service Layer api done

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -6,8 +6,9 @@ import com.example.demo.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
-public class UserServiceImpl  implements UserServiceI {
+public class UserServiceImpl implements UserServiceI {
 
     @Autowired
     private UserRepository userRepository;
@@ -19,26 +20,45 @@ public class UserServiceImpl  implements UserServiceI {
     @Override
     public User createUser(User user) {
         User savedUser = userRepository.save(user);
-        return  savedUser;
+        return savedUser;
     }
 
     @Override
     public User updateUser(User user, Long UserId) {
-        return null;
+
+        User user1 = userRepository.findById(UserId).get();
+        user1.setUserName(user.getUserName());
+        user1.setUserAge(user.getUserAge());
+        user1.setAbout(user.getAbout());
+
+        User updatedUser = userRepository.save(user1);
+        return user1;
     }
 
+    //4th Snehal 28 I am software Dev
     @Override
     public User getSingleUser(Long userId) {
+        userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resourece not found on server"));
         return null;
     }
 
+    /*Optional<User> user= userRepository.findById(userId);
+         if(user.isPresent()) {
+         return user.get();
+     }else{
+         throw new Exception("Resource not found on server"+ userId);
+     }
+     */
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> allUsers = userRepository.findAll();
+        return allUsers;
     }
 
+    //4th snehal , 28, I am software Dev
     @Override
     public void deleteUser(Long userId) {
-
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resourse not found on server" + userId));
+        userRepository.delete(user);
     }
 }
